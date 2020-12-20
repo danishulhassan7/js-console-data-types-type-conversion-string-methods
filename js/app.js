6b21fb04ea8562ea2cea2f4d69571c07078de180
danishulhassan7 <danishulhassan7>
2020-12-20 21:39:13 +0500

    Done with the Variables

--- js/app.js
+++ js/app.js
@@ -1,1 +1,38 @@
 document.write("Hello World");
+
+
+//Variables
+
+var name =  `Danish Ul Hassan`;
+console.log(name);
+
+var marks = 56;
+console.log("Marks " + marks);
+
+let myName = "Malik Dani";
+console.log("Your name is " + myName);
+
+myName = "34";
+console.log("The value is "+ myName);
+
+const age = 21;
+console.log("Your age is "+age);
+
+// age =22;
+// console.log(age);
+// app.js:21 Uncaught TypeError: Assignment to constant variable.
+
+
+ let city = "Karachi";
+ console.log(city);
+
+ {
+     city = "Lahore";
+     console.log(city);
+
+ }
+
+ 
+    
+
+
